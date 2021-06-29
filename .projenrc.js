ad8ee3d7e867d5776958c6ffacb0db3f0ce32550
Neil Guan <guan840912@gmail.com>
2021-06-29 14:40:52 +0800

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,19 @@
+const { AwsCdkConstructLibrary } = require('projen');
+const project = new AwsCdkConstructLibrary({
+  author: 'Neil Guan',
+  authorAddress: 'guan840912@gmail.com',
+  cdkVersion: '1.95.2',
+  defaultReleaseBranch: 'main',
+  name: 'cdk-certbot-dns-route53',
+  repositoryUrl: 'https://github.com/guan840912/cdk-certbot-dns-route53.git',
+
+  // cdkDependencies: undefined,        /* Which AWS CDK modules (those that start with "@aws-cdk/") does this library require when consumed? */
+  // cdkTestDependencies: undefined,    /* AWS CDK modules required for testing. */
+  // deps: [],                          /* Runtime dependencies of this module. */
+  // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],                       /* Build dependencies for this module. */
+  // packageName: undefined,            /* The "name" in package.json. */
+  // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
+  // release: undefined,                /* Add release management to this project. */
+});
+project.synth();
