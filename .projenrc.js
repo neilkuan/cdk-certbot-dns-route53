21db99ec7f9d90ebf5ff33080332a7970404a399
Neil Kuan <guan840912@gmail.com>
2022-01-10 15:41:23 +0800

    chore: deps upgrade

--- .projenrc.js
+++ .projenrc.js
@@ -23,7 +23,6 @@ const project = new awscdk.AwsCdkConstructLibrary({
     ignoreProjen: false,
     workflowOptions: {
       labels: ['auto-approve', 'auto-merge'],
-      secret: 'PROJEN_GITHUB_TOKEN',
     },
   },
   autoApproveOptions: {
